722814143beb0a62b6e7f00ec685ca11b40376ed
woowa-pkch <chulsea@woowahan.com>
2020-04-30 17:01:21 +0900

    [NO-JIRA] images 폴더 assets 하위로 이동

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   siteMetadata: {
     title: `pkch's portpolio`,
@@ -10,7 +12,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: path.join(__dirname, 'src', 'assets', 'images'),
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,12 +26,20 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/assets/images/gatsby-icon.png`,
       },
     },
-    "gatsby-plugin-root-import",
-    "gatsby-plugin-typescript",
-    "gatsby-plugin-styled-components",
+    'gatsby-plugin-root-import',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Nanum Gothic'],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

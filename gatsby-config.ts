207c8fdcd3ad2b6015d099da6e144853747d91b1
pkch93 <dev.pkch@gmail.com>
2026-04-12 20:50:15 +0900

    feature: GA 추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,6 +2,11 @@ import type { GatsbyConfig } from "gatsby";
 import fs from "fs";
 import yaml from "js-yaml";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 // src/data 디렉토리의 모든 YAML 파일들을 읽어와 siteMetadata를 구성합니다.
 const dataDir = "./src/data";
@@ -20,6 +25,16 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      trackingIds: [
+        process.env.GATSBY_GA_MEASUREMENT_ID,
+      ].filter(Boolean) as string[],
+      pluginConfig: {
+        head: true,
+      },
+    },
+  }, {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
